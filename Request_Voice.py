30bb16a606cfc5b02c918ce6ea62e8290e431b4f
Anshulk2004 <anshulk0102@gmail.com>
2024-01-27 19:17:24 +0530

    Uploading Different Voices

---
+++ Request_Voice.py
@@ -0,0 +1,13 @@
+import requests
+
+api_url = "http://127.0.0.1:8000/upload_pdf/"
+files = {'file': open("D:/pdfs/sample1_pdf.pdf", 'rb')}
+params = {'voice_language': 'es-ES', 'voice_gender': 'female'}  # Change the language and gender accordingly
+response = requests.post(api_url, files=files, params=params)
+
+if response.status_code == 200:
+    with open(f"combined_audio.mp3", "wb") as file:
+        file.write(response.content)
+else:
+    print(f"Request failed with status code: {response.status_code}")
+    print(response.text)
---
+++ ToneCraft_Voice.py
@@ -0,0 +1,175 @@
+##Installing the Libraries
+
+import PyPDF2
+import boto3
+import base64
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+from io import BytesIO
+import config
+import os
+import joblib
+
+
+#Running the Fast Api
+app = FastAPI()
+
+class Text(BaseModel):
+    content: str
+    
+##Calling the AWS config settings 
+
+def get_settings():
+    return config.Settings()
+
+##Loading Emotion-Classification Model
+
+pipe_lr = joblib.load(open("./emotion_classifier_pipe_lr.pkl", "rb"))
+
+
+##Predicting Emotion
+
+def predict_emotion(docx):
+    results = pipe_lr.predict([docx])
+    return results[0]
+
+
+##Converting the PDF into text
+
+def pdf_to_text(pdf_content):    
+    temp_pdf_path = "temp_pdf.pdf"
+    with open(temp_pdf_path, "wb") as temp_pdf_file:
+        temp_pdf_file.write(pdf_content)
+       
+    pdf_reader = PyPDF2.PdfReader(temp_pdf_path)
+    text = ''
+    if pdf_reader.pages:
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
+    
+    os.remove(temp_pdf_path)
+    return text
+
+
+##Applying the Basic SSML tags via AMAZON Polly
+
+def applying_basic_polly(text, speaking_rate=1.2, volume='soft', pitch='medium', emphasis='none'):
+    ssml_tags = f"<speak><amazon:effect name='drc'><prosody rate='{speaking_rate}' volume='{volume}' pitch='{pitch}'><emphasis level='{emphasis}'>{text}</emphasis></prosody></amazon:effect></speak>"
+    return ssml_tags
+
+def polly(text, emotion):
+    if emotion == 'anger':
+        text = applying_basic_polly(text, speaking_rate=1.3, volume='loud', emphasis='strong')
+    elif emotion == 'disgust' or emotion == 'sad' or emotion == 'sadness' or emotion == 'shame':
+        text = applying_basic_polly(text, speaking_rate=1.3, volume='soft', pitch='medium', emphasis='reduced')
+    elif emotion == 'happy' or emotion == 'joy':
+        text = applying_basic_polly(text, speaking_rate=1.3, volume='medium', emphasis='moderate')
+    elif emotion == 'fear':
+        text = applying_basic_polly(text, speaking_rate=1.3, volume='medium', pitch='medium', emphasis='strong')
+    elif emotion == 'surprise':
+        text = applying_basic_polly(text, speaking_rate=1.3, volume='medium', emphasis='moderate')
+    elif emotion == 'neutral':
+        text = applying_basic_polly(text, speaking_rate=1.2, volume='soft', pitch='medium', emphasis='none')
+    return text
+
+
+##Generating Audio files
+
+def generate_audio(text, output_format="mp3", voice_language="en-US", voice_gender="male"):
+    # Language and speaker mapping
+    language_speakers = {
+    "en-US": {"male": "Matthew", "female": "Joanna"},
+    "es-ES": {"male": "Enrique", "female": "Conchita"},
+    "hi-IN": {"female": "Aditi"},
+    "de-DE": {"female": "Vicki", "male": "Hans"},
+    "ru-RU": {"female": "Tatyana", "male": "Maxim"},
+    "fr-FR": {"female": "Lea", "male": "Mathieu"},
+}
+
+    # Validate language and gender inputs
+    if voice_language not in language_speakers:
+        raise ValueError("Invalid language. Please choose from: en-US, es-ES, hi-IN, de-DE")
+    
+    if voice_gender not in language_speakers[voice_language]:
+        raise ValueError(f"Invalid gender for the chosen language. Available genders: {', '.join(language_speakers[voice_language].keys())}")
+
+    # Get the voice ID based on language and gender
+    voice_id = language_speakers[voice_language][voice_gender]
+
+    emotion = predict_emotion(text)
+    text = polly(text, emotion)
+
+    client = boto3.client('polly', aws_access_key_id=get_settings().AWS_AK, aws_secret_access_key=get_settings().AWS_SAK, region_name='us-east-1')
+
+    results = client.synthesize_speech(Text=text, OutputFormat=output_format, VoiceId=voice_id, TextType='ssml',Engine='neural')
+    audio = results['AudioStream'].read()
+    encoded_audio = base64.b64encode(audio).decode('utf-8')
+    return encoded_audio
+
+##Storing the chunks in a separate folder called Chunks
+
+def create_audio_folder():
+    # Create a unique folder for each execution
+    folder_name = "audio_folder"
+    folder_path = os.path.join(os.getcwd(), folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    return folder_path
+
+
+##Making a clean audio function
+
+def clean_audio_folder(folder_path):
+    # Delete all files in the specified folder
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                os.rmdir(file_path)
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+##Using the FAST-API Interface
+
+@app.post("/upload_pdf/")
+async def upload_pdf(file: UploadFile = File(...), voice_language: str = "en-US", voice_gender: str = "male"):
+    try:
+        pdf_content = await file.read()
+        pdf_text = pdf_to_text(pdf_content)
+
+        chunk_size = 400
+        text_chunks =  [pdf_text[i:i+chunk_size] for i in range(0, len(pdf_text), chunk_size)]
+
+        # Generate audio for each chunk and save to folder
+        audio_folder = create_audio_folder()
+        audio_files = []
+        for i, chunk in enumerate(text_chunks):
+            audio_data = generate_audio(chunk, voice_language=voice_language, voice_gender=voice_gender)
+
+            # Save each audio chunk to the folder
+            audio_filename = os.path.join(audio_folder, f"audio_chunk_{i + 1}.mp3")
+            audio_files.append(audio_filename)
+            with open(audio_filename, "wb") as audio_file:
+                audio_file.write(base64.b64decode(audio_data))
+
+        combined_audio = b""
+        for audio_file in audio_files:
+            with open(audio_file, "rb") as file:
+                combined_audio += file.read()
+
+        # Return the combined audio using StreamingResponse
+        response = StreamingResponse(iter([combined_audio]), media_type="audio/mpeg", headers={"Content-Disposition": "attachment;filename=combined_audiotw.mp3"})
+
+        # Cleaning the audio folder
+        clean_audio_folder(audio_folder)
+        return response
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {e}")
+
+##END
